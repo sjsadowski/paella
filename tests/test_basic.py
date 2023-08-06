26d8c311e02ae4246816461da5b80496801032b0
Stephen Sadowski <stephen.sadowski@sjsadowski.com>
2023-08-06 09:57:34 -0500

    TEST: stub class initialization tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -2,4 +2,16 @@ import pytest
 from pyaauth import Pyaauth
 
 def test_create_pyaauth():
-    pass
+    assert False
+
+def test_bad_dsn():
+    assert False
+
+def test_good_dsn():
+    assert False
+
+def test_no_dsn():
+    assert False
+
+def test_dsn_late_add():
+    assert False
