e5da29e762615351df8e561977a149fa935c2201
Stephen Sadowski <stephen.sadowski@sjsadowski.com>
2023-08-07 08:34:39 -0500

    TASK: fix test stubs

---
+++ tests/test_authenticate.py
@@ -0,0 +1,13 @@
+import jwt
+
+def test_fail_jwt():
+    assert False
+
+def test_issue_jwt():
+    assert False
+
+def test_fail_jwt_decode():
+    assert False
+
+def test_jwt_decode():
+    assert False
--- tests/test_authorize.py
+++ tests/test_authorize.py
@@ -1,14 +1,2 @@
 import pytest
 
-
-def test_fail_jwt():
-    assert False
-
-def test_issue_jwt():
-    assert False
-
-def test_fail_jwt_decode():
-    assert False
-
-def test_jwt_decode():
-    assert False
