dea7fda34de82354fe1871cb7083014540359293
Stephen Sadowski <stephen.sadowski@sjsadowski.com>
2023-08-07 08:44:43 -0500

    TASK: stub authorization tests

--- tests/test_authorize.py
+++ tests/test_authorize.py
@@ -1,2 +1,17 @@
 import pytest
 
+
+def test_jwt_claim_fail_admin():
+    assert False
+
+
+def test_jwt_claim_admin():
+    assert False
+
+
+def test_jwt_claim_fail_access_area():
+    assert False
+
+
+def test_jwt_claim_access_admin():
+    assert False
