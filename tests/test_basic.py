2a352d3f5eebb4c31badc0c7fd3cfe79fd463f75
Stephen Sadowski <stephen.sadowski@sjsadowski.com>
2023-08-08 07:57:31 -0500

    TASK: update tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,17 +1,28 @@
 import pytest
 from pyaauth import Pyaauth
 
+def authn() -> bool:
+    return True
+
+
+def authz() -> bool:
+    return True
+
+
+def test_create_pyaauth_no_authx_fns():
+    pya = Pyaauth()
+    assert isinstance(pya, Pyaauth)
+
+def test_create_pyaauth_no_authn_fn():
+    pya = Pyaauth(authz_fn=authz)
+    assert isinstance(pya, Pyaauth)
+
+
+def test_create_pyaauth_no_authz_fn():
+    pya = Pyaauth(authn_fn=authn)
+    assert isinstance(pya, Pyaauth)
+
+
 def test_create_pyaauth():
-    assert False
-
-def test_bad_dsn():
-    assert False
-
-def test_good_dsn():
-    assert False
-
-def test_no_dsn():
-    assert False
-
-def test_dsn_late_add():
-    assert False
+    pya = Pyaauth(authn_fn=authn, authz_fn=authz)
+    assert isinstance(pya, Pyaauth)
