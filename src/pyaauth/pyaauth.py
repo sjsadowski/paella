16316b8f9cc34d23d555f637dc34dfbfc0e59d70
Stephen Sadowski <stephen.sadowski@sjsadowski.com>
2023-08-08 08:37:16 -0500

    TASK: refactor properties

--- src/pyaauth/pyaauth.py
+++ src/pyaauth/pyaauth.py
@@ -1,39 +1,89 @@
 import asyncio
-from typing import Callable
+from typing import Callable, Any
 import jwt
 
 class Pyaauth:
 
     __slots__ = [
-        'authn_fn',
-        'authz_fn'
+        '_authn_fn',
+        '_authz_fn',
+        '_pubkey',
+        '_privkey',
+        '_cxobj'
     ]
 
-    def __init__(self, authn_fn=None, authz_fn=None) -> None:
-        self.set_authn_fn(authn_fn)
-        self.set_authz_fn(authz_fn)
-
-
-    def set_authn_fn(self, authn_fn: Callable | None) -> None:
+    def __init__(self, authn_fn=None, authz_fn=None, cxobj=None, privkey=None, pubkey=None) -> None:
         self.authn_fn = authn_fn
-
-
-    def set_authz_fn(self, authz_fn: Callable | None) -> None:
         self.authz_fn = authz_fn
+
+
+    @property
+    def authn_fn(self) -> Callable | None:
+        return self._authn_fn
+
+    @authn_fn.setter
+    def authn_fn(self, authn_fn: Callable | None) -> None:
+        self._authn_fn = authn_fn
+
+    @property
+    def authz_fn(self) -> Callable | None:
+        return self._authz_fn
+
+    @authz_fn.setter
+    def authz_fn(self, authz_fn: Callable | None) -> None:
+        self._authz_fn = authz_fn
+
+    @property
+    def pubkey(self) -> str | None:
+        return self._pubkey
+
+    @pubkey.setter
+    def pubkey(self, pubkey: str | None) -> None:
+        self._pubkey = pubkey
+
+    @property
+    def privkey(self) -> str | None:
+        return self._privkey
+
+    @privkey.setter
+    def privkey(self, privkey: str | None) -> None:
+        self._privkey = privkey
+
+
+    @property
+    def cxobj(self) -> Any:
+        return self._cxobj
+
+    @cxobj.setter
+    def privkey(self, cxobj: Any) -> None:
+        self._cxobj = cxobj
 
 
     # If authenticated, returns jwt or None
     async def authenticate(self, id: str, secret: str) -> str | bool:
+
+        # default: not authenticated
+        authn: bool = False
+
         if self.authn_fn is None:
             raise NotImplementedError('No authentication function is set')
 
         if asyncio.isawaitable(self.authn_fn):
-            await self.authn_fn(id, secret)
+            authn = await self.authn_fn(id, secret)
         else:
-            self.authn_fn(id, secret)
+            authn = self.authn_fn(id, secret)
 
-    # Authorization
-    async def authorize(self, jwt: str, claim: str, value: str) -> str | bool:
+        if authn:
+            pass
+
+        return authn
+
+    # Authorization - note, this only validates a sig/checks a claim
+    async def authorize(self, jwt: str = '', claim: str = '', value: str = '') -> str | bool:
+
+        # default: unauthorized
+        authz: bool = False
+
         if self.authz_fn is None:
             raise NotImplementedError('No authorization function is set')
 
@@ -43,3 +93,5 @@ class Pyaauth:
         else:
             self.authn_fn(jwt, claim, value)
 
+
+        return authz
