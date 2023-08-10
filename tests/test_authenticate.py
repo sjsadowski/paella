ed7eecf9bfcd557df0a4ed1a34f47710300dc9ab
Stephen Sadowski <stephen.sadowski@sjsadowski.com>
2023-08-10 13:35:13 -0500

    TEST: update authn tests/fixtures.

--- tests/test_authenticate.py
+++ tests/test_authenticate.py
@@ -1,15 +1,18 @@
 import pytest
 
+import aiosqlite
+import sqlite3
 import jwt
+
 from paella import Paella
 
 @pytest.fixture
 def sql3_sync_obj():
-    return True
+    return sqlite3.connect("./tests/db/test_users.db")
 
 @pytest.fixture
 def sql3_async_obj():
-    return True
+    return aiosqlite.connect("./tests/db/test_users.db")
 
 @pytest.fixture
 def paella_auth():
@@ -21,6 +24,19 @@ async def test_fail_no_authn_fn(paella_auth):
     with pytest.raises(NotImplementedError):
         await paella_auth.authenticate()
 
+
+@pytest.mark.asyncio
+async def test_async_authenticate_basic(paella_auth, sql3_async_obj):
+    assert False
+
+# Providing clarity for the below test:
+# The Paella.authenticate() function is asynchronous
+# But for ease of use you can pass a standard (synchronous) function
+# Just testing to make sure both work as expected, even
+# though the synchronous function will block.
+@pytest.mark.asyncio
+async def test_authenticate_basic(paella_auth, sql3_async_obj):
+    assert False
 
 def test_fail_jwt():
     assert False
